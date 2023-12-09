296f410a6b4b1eca1339b30cc0f91397ef49416a
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-09 11:59:03 +0100

    Update ContactRepository.cs

--- CompanyApp.DataAccess/Implementations/ContactRepository.cs
+++ CompanyApp.DataAccess/Implementations/ContactRepository.cs
@@ -10,51 +10,51 @@ using System.Threading.Tasks;
 
 namespace CompanyApp.DataAccess.Implementations
 {
-	public class ContactRepository : IRepository<Contact>
-	{
-		private readonly CompanyAppDbContext _context;
+     public class ContactRepository : IRepository<Contact>
+     {
+	 private readonly CompanyAppDbContext _context;
 
-        public ContactRepository(CompanyAppDbContext context)
-        {
-			_context = context;
-        }
+         public ContactRepository(CompanyAppDbContext context)
+         {
+	     _context = context;
+         }
 
-        public async Task CreateAsync(Contact entity)
-		{
-			await _context.Contact.AddAsync(entity);
-			await _context.SaveChangesAsync();
-		}
+         public async Task CreateAsync(Contact entity)
+	 {
+		await _context.Contact.AddAsync(entity);
+		await _context.SaveChangesAsync();
+	 }
 
-		public async Task DeleteAsync(int? id)
-		{
-			Contact contactDb = await GetByIdAsync(id);
+	 public async Task DeleteAsync(int? id)
+	 {
+		Contact contactDb = await GetByIdAsync(id);
 
-			_context.Remove(contactDb);
-		}
+		_context.Remove(contactDb);
+	 }
 
-		public List<Contact> FilterContacts (int countryId, int companyId)
-		{
-			if(companyId == 0 && countryId == 0)
-			{
-				return _context.Contact.ToList();
-			}
+	  public List<Contact> FilterContacts (int countryId, int companyId)
+	  {
+	      if(companyId == 0 && countryId == 0)
+	      {
+		   return _context.Contact.ToList();
+	      }
 
-			if(companyId == 0)
-			{
-				List<Contact> contactDb = _context.Contact.Where(x=>x.CountryId == countryId).ToList();
-				return contactDb;
-			}
+	      if(companyId == 0)
+	      {
+			List<Contact> contactDb = _context.Contact.Where(x=>x.CountryId == countryId).ToList();
+			return contactDb;
+	      }
 
-			if (countryId == 0)
-			{
-				List<Contact> companyDb = _context.Contact.Where(x => x.CompanyId == companyId).ToList();
-				return companyDb;
-			}
+	      if (countryId == 0)
+	      {
+		    List<Contact> companyDb = _context.Contact.Where(x => x.CompanyId == companyId).ToList();
+		    return companyDb;
+	      }
 
-			List<Contact> contacts = _context.Contact
-				                      .Where(x=>x.CompanyId == companyId && x.CountryId == countryId).ToList();
+		List<Contact> contacts = _context.Contact.Where(x=>x.CompanyId == companyId && x.CountryId == countryId).ToList();
 
-			return contacts;
+		return contacts;
+  
 		}
 
 		public async Task<List<Contact>> GetAllAsync()
