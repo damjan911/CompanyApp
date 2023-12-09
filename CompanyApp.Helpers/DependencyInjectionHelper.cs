8687c3a7df4232144371047307ba35ac7ee728a7
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-09 12:15:41 +0100

    Update DependencyInjectionHelper.cs

--- CompanyApp.Helpers/DependencyInjectionHelper.cs
+++ CompanyApp.Helpers/DependencyInjectionHelper.cs
@@ -14,25 +14,25 @@ using System.Threading.Tasks;
 
 namespace CompanyApp.Helpers
 {
-	public static class DependencyInjectionHelper
+    public static class DependencyInjectionHelper
+    {
+	public static void InjectDbContext(this IServiceCollection services)
 	{
-		public static void InjectDbContext(this IServiceCollection services)
-		{
-			services.AddDbContext<CompanyAppDbContext>(options => options.UseSqlServer("Server=(localdb)\\MSSQLServer;Database=CompanyDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
-		}
-
-		public static void InjectRepositories(this IServiceCollection services)
-		{
-			services.AddTransient<IRepository<Company>,CompanyRepository>();
-			services.AddTransient<IRepository<Contact>,ContactRepository>();
-			services.AddTransient<IRepository<Country>,CountryRepository>();
-		}
-
-		public static void InjectServices(this IServiceCollection services)
-		{
-			services.AddTransient<ICompanyService, CompanyService>();
-			services.AddTransient<IContactService, ContactService>();
-			services.AddTransient<ICountryService, CountryService>();
-		}
+	    services.AddDbContext<CompanyAppDbContext>(options => options.UseSqlServer("Server=(localdb)\\MSSQLServer;Database=CompanyDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
 	}
+
+	public static void InjectRepositories(this IServiceCollection services)
+	{
+		services.AddTransient<IRepository<Company>,CompanyRepository>();
+		services.AddTransient<IRepository<Contact>,ContactRepository>();
+		services.AddTransient<IRepository<Country>,CountryRepository>();
+	}
+
+	public static void InjectServices(this IServiceCollection services)
+	{
+		services.AddTransient<ICompanyService, CompanyService>();
+		services.AddTransient<IContactService, ContactService>();
+		services.AddTransient<ICountryService, CountryService>();
+	}
+    }
 }
