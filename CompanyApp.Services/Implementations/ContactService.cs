e01844f3c9ea756b6dcfdb84164a02a71e17ddee
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-09 12:48:00 +0100

    Update ContactService.cs

--- CompanyApp.Services/Implementations/ContactService.cs
+++ CompanyApp.Services/Implementations/ContactService.cs
@@ -12,63 +12,63 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace CompanyApp.Services.Implementations
 {
-	public class ContactService : IContactService
+     public class ContactService : IContactService
+     {
+	 private readonly IRepository<Contact> _contactRepository;
+
+      public ContactService(IRepository<Contact> contactRepository)
+      {
+	      _contactRepository = contactRepository;
+      }
+
+      public async Task CreateContactAsync(CreateContactDto createContactDto)
+      {
+		Contact contactEntity = createContactDto.MapToContact();
+
+		await _contactRepository.CreateAsync(contactEntity);
+      }
+
+       public async Task DeleteContactAsync(int? id)
+       {
+		await _contactRepository.DeleteAsync(id);
+       }
+
+	public async Task<IEnumerable<ContactDto>> GetAllContactsAsync()
 	{
-		private readonly IRepository<Contact> _contactRepository;
+		List<Contact> contact = await _contactRepository.GetAllAsync();
 
-        public ContactService(IRepository<Contact> contactRepository)
-        {
-			_contactRepository = contactRepository;
-        }
-
-        public async Task CreateContactAsync(CreateContactDto createContactDto)
+		if (contact == null)
 		{
-			Contact contactEntity = createContactDto.MapToContact();
-
-			await _contactRepository.CreateAsync(contactEntity);
+			throw new NotImplementedException("Contact is null.");
 		}
 
-		public async Task DeleteContactAsync(int? id)
+		   return contact.Select(contact => contact.MapToContactDto()).ToList();
+	}
+
+	  public async Task<ContactDto> GetContactByIdAsync(int? id)
+	  {
+		  Contact contact = await _contactRepository.GetByIdAsync(id);
+
+		  if (contact == null)
+		  {
+			 throw new NotImplementedException("Contact is null.");
+		  }
+                return contact.MapToContactDto();
+	   }
+
+	  public async Task UpdateContactAsync(CreateContactDto createContactDto, int? id)
+	  {
+		Contact contactDb = await _contactRepository.GetByIdAsync(id);
+
+		if(contactDb == null)
 		{
-			await _contactRepository.DeleteAsync(id);
+			throw new NotImplementedException("Contact is null");
 		}
 
-		public async Task<IEnumerable<ContactDto>> GetAllContactsAsync()
-		{
-			List<Contact> contact = await _contactRepository.GetAllAsync();
-
-			if (contact == null)
-			{
-				throw new NotImplementedException("Contact is null.");
-			}
-
-			return contact.Select(contact => contact.MapToContactDto()).ToList();
-		}
-
-		public async Task<ContactDto> GetContactByIdAsync(int? id)
-		{
-			Contact contact = await _contactRepository.GetByIdAsync(id);
-
-			if (contact == null)
-			{
-				throw new NotImplementedException("Contact is null.");
-			}
-             return contact.MapToContactDto();
-		}
-
-		public async Task UpdateContactAsync(CreateContactDto createContactDto, int? id)
-		{
-			Contact contactDb = await _contactRepository.GetByIdAsync(id);
-
-			if(contactDb == null)
-			{
-				throw new NotImplementedException("Contact is null");
-			}
-
-			contactDb.ContactName = createContactDto.ContactName;
-			contactDb.JobTitle = createContactDto.JobTitle;
-			contactDb.CompanyId = createContactDto.CompanyId;
-			contactDb.CountryId = createContactDto.CountryId;
-		}
+		   contactDb.ContactName = createContactDto.ContactName;
+		   contactDb.JobTitle = createContactDto.JobTitle;
+		   contactDb.CompanyId = createContactDto.CompanyId;
+		   contactDb.CountryId = createContactDto.CountryId;
+	   }
 	}
 }
