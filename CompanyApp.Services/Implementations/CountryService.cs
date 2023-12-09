cea512be56a2db45452c8067732c9d82309476e1
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-09 12:50:41 +0100

    Update CountryService.cs

--- CompanyApp.Services/Implementations/CountryService.cs
+++ CompanyApp.Services/Implementations/CountryService.cs
@@ -11,60 +11,61 @@ using System.Threading.Tasks;
 
 namespace CompanyApp.Services.Implementations
 {
-	public class CountryService : ICountryService
-	{
-		private readonly IRepository<Country> _countryRepository;
+     public class CountryService : ICountryService
+     {
+	  private readonly IRepository<Country> _countryRepository;
 
-        public CountryService(IRepository<Country> countryRepository)
-        {
-			_countryRepository = countryRepository;
-        }
-        public async Task CreateCountryAsync(CreateCountryDto createCountryDto)
-		{
-			Country countryEntity = createCountryDto.MapToCountry();
+          public CountryService(IRepository<Country> countryRepository)
+          {
+		 _countryRepository = countryRepository;
+          }
+	  
+          public async Task CreateCountryAsync(CreateCountryDto createCountryDto)
+	  {
+		 Country countryEntity = createCountryDto.MapToCountry();
 
-			await _countryRepository.CreateAsync(countryEntity);
-		}
+		 await _countryRepository.CreateAsync(countryEntity);
+	  }
 
-		public async Task DeleteCountryAsync(int? id)
-		{
-			await _countryRepository.DeleteAsync(id);
-		}
+	  public async Task DeleteCountryAsync(int? id)
+	  {
+		 await _countryRepository.DeleteAsync(id);
+	  }
 
-		public async Task<IEnumerable<CountryDto>> GetAllCountriesAsync()
-		{
-			List<Country> country = await _countryRepository.GetAllAsync();
+	  public async Task<IEnumerable<CountryDto>> GetAllCountriesAsync()
+	  {
+		  List<Country> country = await _countryRepository.GetAllAsync();
 
-			if(country == null)
-			{
-				throw new NotImplementedException("Country is null.");
-			}
+		  if(country == null)
+		  {
+			 throw new NotImplementedException("Country is null.");
+		  }
 
-			return country.Select(country => country.MapToCountryDto()).ToList();
-		}
+		 return country.Select(country => country.MapToCountryDto()).ToList();
+	  }
 
-		public async Task<CountryDto> GetCountryByIdAsync(int? id)
-		{
-			Country country = await _countryRepository.GetByIdAsync(id);
+	  public async Task<CountryDto> GetCountryByIdAsync(int? id)
+	  {
+		   Country country = await _countryRepository.GetByIdAsync(id);
 
-			if(country == null)
-			{
-				throw new NotImplementedException("Country is null.");
-			}
+		   if(country == null)
+		   {
+			 throw new NotImplementedException("Country is null.");
+		   }
 
-			return country.MapToCountryDto();
-		}
+		  return country.MapToCountryDto();
+	   }
 
-		public async Task UpdateCountryAsync(CreateCountryDto createCountryDto, int? id)
-		{
-			Country countryDb = await _countryRepository.GetByIdAsync(id);
+	   public async Task UpdateCountryAsync(CreateCountryDto createCountryDto, int? id)
+	   {
+		   Country countryDb = await _countryRepository.GetByIdAsync(id);
 
-			if( countryDb == null)
-			{
-				throw new NotImplementedException("Country is null");
-			}
+		   if( countryDb == null)
+		   {
+			  throw new NotImplementedException("Country is null");
+		   }
 
-			countryDb.CountryName = createCountryDto.CountryName;
-		}
-	}
+		   countryDb.CountryName = createCountryDto.CountryName;
+	   }
+	 }
 }
