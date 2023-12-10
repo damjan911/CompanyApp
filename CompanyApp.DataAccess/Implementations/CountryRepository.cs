5a0ae07940cda0d56578067d1305d6d864a7c7f4
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-10 21:26:23 +0100

    Update CountryRepository.cs

--- CompanyApp.DataAccess/Implementations/CountryRepository.cs
+++ CompanyApp.DataAccess/Implementations/CountryRepository.cs
@@ -13,43 +13,42 @@ namespace CompanyApp.DataAccess.Implementations
      {
 	 private readonly CompanyAppDbContext _dbContext;
 
-     public CountryRepository(CompanyAppDbContext dbContext)
-     {
-	  _dbContext = dbContext;
-     }
+         public CountryRepository(CompanyAppDbContext dbContext)
+         {
+	      _dbContext = dbContext;
+         }
 
-     public async Task CreateAsync (Country entity)
-     {
-	  await _dbContext.Country.AddAsync(entity);
-	  await _dbContext.SaveChangesAsync();
-     }
+         public async Task CreateAsync (Country entity)
+         {
+	       await _dbContext.Country.AddAsync(entity);
+	       await _dbContext.SaveChangesAsync();
+         }
 
+         public async Task DeleteAsync(int? id)
+         {
+	      Country countryDb = await GetByIdAsync(id);
 
-     public async Task DeleteAsync(int? id)
-     {
-	   Country countryDb = await GetByIdAsync(id);
+	      _dbContext.Remove(countryDb);
 
-			_dbContext.Remove(countryDb);
+	     await _dbContext.SaveChangesAsync();
 
-			await _dbContext.SaveChangesAsync();
+         }
 
-		}
+         public async Task<List<Country>> GetAllAsync()
+         {
+	       return await _dbContext.Country.ToListAsync();
+         }
 
-     public async Task<List<Country>> GetAllAsync()
-     {
-	  return await _dbContext.Country.ToListAsync();
-     }
+         public async Task<Country> GetByIdAsync(int? id)
+         {
+              return await _dbContext.Country.FirstOrDefaultAsync(x => x.Id == id);
+         }
 
-     public async Task<Country> GetByIdAsync(int? id)
-     {
-          return await _dbContext.Country.FirstOrDefaultAsync(x => x.Id == id);
-     }
-
-     public async Task UpdateAsync(Country entity)
-      {
-	  _dbContext.Country.Update(entity);
-	  await _dbContext.SaveChangesAsync();
-      }
+         public async Task UpdateAsync(Country entity)
+         {
+	        _dbContext.Country.Update(entity);
+	       await _dbContext.SaveChangesAsync();
+         }
       
      }
 }
