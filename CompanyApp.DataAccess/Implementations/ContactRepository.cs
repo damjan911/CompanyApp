49c9f5b099deb086b96fa01f1c67a765eda9eb84
Damjan <119892938+damjan911@users.noreply.github.com>
2023-12-10 21:50:19 +0100

    Update ContactRepository.cs

--- CompanyApp.DataAccess/Implementations/ContactRepository.cs
+++ CompanyApp.DataAccess/Implementations/ContactRepository.cs
@@ -27,12 +27,12 @@ namespace CompanyApp.DataAccess.Implementations
 
 	 public async Task DeleteAsync(int? id)
 	 {
-		Contact contactDb = await GetByIdAsync(id);
+		 Contact contactDb = await GetByIdAsync(id);
 
-			_context.Remove(contactDb);
+		 _context.Remove(contactDb);
 
-			await _context.SaveChangesAsync();
-		}
+		 await _context.SaveChangesAsync();
+	 }
 
 	  public List<Contact> FilterContacts (int countryId, int companyId)
 	  {
@@ -49,30 +49,30 @@ namespace CompanyApp.DataAccess.Implementations
 
 	      if (countryId == 0)
 	      {
-		    List<Contact> companyDb = _context.Contact.Where(x => x.CompanyId == companyId).ToList();
-		    return companyDb;
+		       List<Contact> companyDb = _context.Contact.Where(x => x.CompanyId == companyId).ToList();
+		       return companyDb;
 	      }
 
 		List<Contact> contacts = _context.Contact.Where(x=>x.CompanyId == companyId && x.CountryId == countryId).ToList();
 
 		return contacts;
   
-		}
+	   }
 
-		public async Task<List<Contact>> GetAllAsync()
-		{
-			return await _context.Contact.ToListAsync();
-		}
+	   public async Task<List<Contact>> GetAllAsync()
+	   {
+		 return await _context.Contact.ToListAsync();
+	   }
 
-		public async Task<Contact> GetByIdAsync(int? id)
-		{
-			return await _context.Contact.FirstOrDefaultAsync(x=>x.Id == id);
-		}
+	   public async Task<Contact> GetByIdAsync(int? id)
+	   {
+		 return await _context.Contact.FirstOrDefaultAsync(x=>x.Id == id);
+	   }
 
-		public async Task UpdateAsync(Contact entity)
-		{
-			_context.Contact.Update(entity);
-			await UpdateAsync(entity);
-		}
+	   public async Task UpdateAsync(Contact entity)
+	   {
+		  _context.Contact.Update(entity);
+		  await UpdateAsync(entity);
+	   }
 	}
 }
